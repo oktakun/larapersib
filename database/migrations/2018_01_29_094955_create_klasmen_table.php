93260924a945ffc142ad23a2f207680ee6eeef6a
oktakun <oktafirdaus231111@gmail.com>
2018-02-02 15:29:24 +0700

    adding clasment controller

--- database/migrations/2018_01_29_094955_create_klasmen_table.php
+++ database/migrations/2018_01_29_094955_create_klasmen_table.php
@@ -13,8 +13,25 @@ class CreateKlasmenTable extends Migration
      */
     public function up()
     {
-        //
+      Schema::create('klasmen', function (Blueprint $table) {
+
+          $table->increments('id');
+          $table->string('liga');
+          $table->string('klub');
+          $table->string('images');
+          $table->string('main');
+          $table->string('menang');
+          $table->string('seri');
+          $table->string('kalah');
+          $table->string('goal_cetak');
+          $table->string('goal_masuk');
+          $table->string('poin');
+          $table->timestamps();
+
+      });
     }
+
+
 
     /**
      * Reverse the migrations.
@@ -23,6 +40,6 @@ class CreateKlasmenTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('klasmen');
     }
 }
